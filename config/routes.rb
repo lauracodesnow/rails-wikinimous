67f4ddac5accf8352f2e0ebe5f9482601b3fe5bf
Laura Gil <lauracodesnow@gmail.com>
2022-05-17 16:40:06 +0200

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/articles', to: 'articles#index', as: 'articles'
+  get '/articles/new', to: 'articles#new'
+  get '/articles/:id', to: 'articles#show', as: 'article'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit'
+  post '/articles', to: 'articles#create'
+  patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy', as: 'delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
